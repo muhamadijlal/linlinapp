509bd8231957a3cf89563fc8803eaaf189db7cfc
muhamadijlal <haidarijlal027@gmail.com>
2023-08-13 19:35:39 +0700

    finish desain dashboard

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import { createApp } from "vue";
 import "@/style.css";
 import App from "@/App.vue";
 import router from "@/routes.js";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas, far);
 
 const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 app.mount("#app");
