6d224896958f23cc6c209d1bef1d224b678e60f6
muhamadijlal <haidarijlal027@gmail.com>
2023-08-15 21:34:38 +0700

    make changes on navbar and footer

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,9 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const isHidden = ref(true);
+</script>
+
 <template>
   <nav>
     <div class="bg-[#032541] py-4">
       <ul class="flex justify-between px-5 items-center">
         <li class="text-white">
-          <font-awesome-icon :icon="['fas', 'bars']" />
+          <button @click="isHidden = !isHidden">
+            <font-awesome-icon :icon="['fas', 'bars']" />
+          </button>
         </li>
         <li class="ml-10">
           <img
@@ -22,4 +30,25 @@
       </ul>
     </div>
   </nav>
+
+  <div
+    class="bg-[#032541] bg-opacity-90 w-[90%] backdrop-blur-md z-10 fixed h-full p-5"
+    :class="{ hidden: isHidden }"
+  >
+    <div class="space-y-2">
+      <h4 class="text-white text-xl font-semibold">Film</h4>
+      <h4 class="text-white text-xl font-semibold">Serial TV</h4>
+      <h4 class="text-white text-xl font-semibold">Orang</h4>
+    </div>
+    <div class="py-4 space-y-2">
+      <p class="font-bold text-gray-400 text-base">Panduan Kontribusi</p>
+      <p class="font-bold text-gray-400 text-base">Diskusi</p>
+      <p class="font-bold text-gray-400 text-base">Papan Peringkat</p>
+      <p class="font-bold text-gray-400 text-base">API</p>
+      <p class="font-bold text-gray-400 text-base">Dukungan</p>
+      <p class="font-bold text-gray-400 text-base">Tentang</p>
+    </div>
+
+    <h5 class="text-base font-bold text-gray-400">Masuk</h5>
+  </div>
 </template>
