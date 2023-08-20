cd59ab2f18a82cf4b232e734759b2abb008d8061
muhamadijlal <haidarijlal027@gmail.com>
2023-08-20 22:00:00 +0700

    feature: feature fetch data movie trending, popular, and watch from API tmdb

--- src/components/movies/ListMovies.vue
+++ src/components/movies/ListMovies.vue
@@ -1,14 +1,24 @@
 <script setup>
-import Percentage from "@/components/movies/Percentage.vue"
+import Percentage from "@/components/movies/Percentage.vue";
+import moment from "moment";
+
+defineProps({
+  movie: Object,
+});
 </script>
 
 <template>
   <div class="h-80">
     <div
       class="w-40 h-2/3 bg-cover rounded-md mb-6 relative"
-      style="background-image: url('/src/assets/img/carousel.jpg')"
+      :style="{
+        'background-image':
+          'url(https://image.tmdb.org/t/p/w220_and_h330_face' +
+          movie.poster_path +
+          ')',
+      }"
     >
-      <Percentage/>
+      <Percentage :percent="100" />
 
       <div
         class="bg-gray-200 rounded-full absolute right-2 top-2 opacity-50 w-5 h-5 flex items-center justify-center hover:bg-sky-500"
@@ -20,7 +30,9 @@ import Percentage from "@/components/movies/Percentage.vue"
       </div>
     </div>
 
-    <h5 class="text-base font-bold">No Hard feelings</h5>
-    <p class="text-md font-base text-slate-500">Jun 15, 2023</p>
+    <h5 class="text-base font-bold">{{ movie.original_title }}</h5>
+    <p class="text-md font-base text-slate-500">
+      {{ moment(movie.release_date).format("LL") }}
+    </p>
   </div>
 </template>
